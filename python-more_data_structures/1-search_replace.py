624a716eb7165161e5b3d74e6d8752da1fe4d8c1
Nijat <hatamlinijat@gmail.com>
2025-10-28 16:39:06 +0400

    search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list.copy()
-    for i,x in enumerate(my_list):
+    for i, x in enumerate(my_list):
         if x == search:
             new[i] = replace
     return new
