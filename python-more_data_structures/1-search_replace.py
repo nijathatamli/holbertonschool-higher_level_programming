5e8f8d9a2180028039a9f97b4794827dddde2f59
Nijat <hatamlinijat@gmail.com>
2025-10-28 16:37:02 +0400

    search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    index_of_search = my_list.index(search)
-    index_of_replace = my_list.index(replace)
-    my_list[index_of_search], my_list[index_of_replace] = my_list[index_of_replace], my_list[index_of_search]
+    for i,x in enumerate(my_list):
+        if x == search:
+            my_list[i] = replace
     return my_list
