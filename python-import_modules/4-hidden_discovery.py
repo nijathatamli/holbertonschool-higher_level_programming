03727951df42ccfb01dfc42f57f7f4fed3c385c3
Nijat <hatamlinijat@gmail.com>
2025-10-28 13:25:26 +0400

    who am i

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
+
 if __name__ == "__main__":
-    hidden_names = dir(hidden_4)
-    for i in hidden_names:
-        if i.startswith("__") == False:
-            print(i)
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
