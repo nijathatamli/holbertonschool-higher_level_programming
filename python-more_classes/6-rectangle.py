4744fd7450803523f4fcad80a2c9204287efec27
Nijat <hatamlinijat@gmail.com>
2025-11-22 14:52:46 +0400

    just fixed bug in 6th task

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,9 +7,11 @@
 class Rectangle:
     """This class represents a rectangle"""
 
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1 
 
     @property
     def width(self):
