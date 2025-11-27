8f50d98eb001d2ee25b3c230fbaaf06e16bebdbc
Nijat <hatamlinijat@gmail.com>
2025-11-27 21:50:19 +0400

    average of all scores

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-select AVG(scores) FROM secon_table;
+SELECT AVG(scores) FROM secon_table;
