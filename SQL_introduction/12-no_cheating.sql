fac21a8f6bd07b18812be9b6dec020168adb9bec
Nijat <hatamlinijat@gmail.com>
2025-11-27 21:37:21 +0400

    all structures changed for correct answer

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- that updates the score of Bob to 10 in the table second_table.
-SELECT score, name FROM second_table
-WHERE 10 =< score
-ORDER BY score DESC;
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
