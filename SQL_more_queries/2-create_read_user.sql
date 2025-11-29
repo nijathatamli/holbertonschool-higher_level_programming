66c9a90cab1722dcf9ce6aaedc748554b9573a98
Nijat <hatamlinijat@gmail.com>
2025-11-29 15:49:27 +0400

    thats show priveleges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- that creates the database hbtn_0d_2 and the user user_0d_2. 
+CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT hbtn_0d_2.* TO 'user_0d_2'@'localhost';
