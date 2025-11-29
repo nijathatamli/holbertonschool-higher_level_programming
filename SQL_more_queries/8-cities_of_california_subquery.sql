0f02b5fb1f0ad05293a0cd57170f97c5ea6ceb1a
Nijat <hatamlinijat@gmail.com>
2025-11-29 21:09:59 +0400

    fixed syntax erors

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- that lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT id, name FROM cities
-WHERE state_id =(SELECT id FROM states WHERE name = 'Califfornia') ORDER BY ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
