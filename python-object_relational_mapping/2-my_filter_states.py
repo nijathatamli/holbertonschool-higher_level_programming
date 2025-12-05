7c409c514923064810245b51e4e4fd675b0c5e62
Nijat <hatamlinijat@gmail.com>
2025-12-06 02:00:22 +0400

    cha

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,14 +4,16 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-    
+
     db = MySQLdb.connect(
         user=argv[1],
         password=argv[2],
         database=argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY id".format(argv[4]))
+    cursor.execute("SELECT * FROM `states`" \
+    " WHERE BINARY `name` = '{}'" \
+    " ORDER BY id".format(argv[4]))
     for x in cursor.fetchall():
         print(x)
     if cursor:
