dd2d41e70de02a4fa991536bf3c8ab6e4bff2d5a
Nijat <hatamlinijat@gmail.com>
2025-12-06 01:06:13 +0400

    thats exsists wanted element of id

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""""that takes in an argument and displays all values"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    
+    db = MySQLdb.cursors(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY id".format(argv[4]))
+    for x in cursor.fetchall():
+        print(x)
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
