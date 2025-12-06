e086b6477233159f05f847326a91cd0c9837e8e1
Nijat <hatamlinijat@gmail.com>
2025-12-07 00:57:36 +0400

    fixed syntax problem for testing

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,18 +4,23 @@
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb
+
     db = MySQLdb.connect(
         user=argv[1],
         password=argv[2],
         database=argv[3]
     )
+
     cursor = db.cursor()
-    cursor.execute("SELECT *  \
-                        FROM `states` \
-                        ORDER BY id")
-    for x in cursor.fetchall():
-        if x[1] == argv[4]:
-            print(x)
+
+    cursor.execute("SELECT * \
+                    FROM `states` \
+                    ORDER BY id")
+
+    for state in cursor.fetchall():
+        if state[1] == argv[4]:
+            print(state)
+
     if cursor:
         cursor.close()
     if db:
