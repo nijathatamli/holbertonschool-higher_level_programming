45c3c859fdfd21adeb2fe755653d68ecf679bab2
Nijat <hatamlinijat@gmail.com>
2025-12-12 13:35:35 +0400

    txt file was wrriten by me

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for lookup function"""
+
+
 class MyList(list):
+    """Class inherited from list class"""
     def print_sorted(self):
         new = sorted(self)
         print(new)
