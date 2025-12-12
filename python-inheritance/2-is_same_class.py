5baab496bfb7772e0220d041922452d710321f47
Nijat <hatamlinijat@gmail.com>
2025-12-12 13:41:33 +0400

    that script check obj is in a_class or not

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Modul look at sae class or not"""
+
+
+def is_same_class(obj, a_class):
+    """object is exactly an instance of the specified class"""    
+    return obj is a_class
