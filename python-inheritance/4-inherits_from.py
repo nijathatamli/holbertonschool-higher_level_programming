97bd426631c654a34e5f54b5cb2f3e11be3d9172
Nijat <hatamlinijat@gmail.com>
2025-12-13 20:25:26 +0400

    that script check this obj is coming from class or just own by itself

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""""That modul chech inheritence or not"""
+def inherits_from(obj, a_class):
+    """used isinstance and is not"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
