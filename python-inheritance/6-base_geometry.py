39dd7e99d04d27a1db055dc65c6cce12acf2fa12
Nijat <hatamlinijat@gmail.com>
2025-12-13 20:40:22 +0400

    when area is not implemented, modul stop all program

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Enhance BaseGeometry class"""
+
+
+class BaseGeometry:
+    """added area fucntion"""
+    def area(self):
+        """modul raise program"""
+        raise Exception("area() is not implemented")
