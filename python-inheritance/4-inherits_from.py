9f6d44a17db87a41fcd049bf1d601f24b0eb186d
Nijat <hatamlinijat@gmail.com>
2025-12-13 20:27:44 +0400

    just we created empty class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """""That modul chech inheritence or not"""
+
+
 def inherits_from(obj, a_class):
     """used isinstance and is not"""
     return isinstance(obj, a_class) and type(obj) is not a_class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""""Just we are gonna write emtyu class."""
+
+
+class BaseGeometry:
+    pass
