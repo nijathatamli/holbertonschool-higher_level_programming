71bfd7c7f6402d1ba14a24ad0d6622e87c59251f
Nijat <hatamlinijat@gmail.com>
2025-12-14 16:48:18 +0400

    pycode style fixed an again

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
     def area(self):
         """Method to raise exception"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Method to validate integer"""
         if type(value) is not int:
