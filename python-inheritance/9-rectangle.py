40b40e4b9582ade807872e9d66de68f9a28c01d9
Nijat <hatamlinijat@gmail.com>
2025-12-14 21:40:41 +0400

    complated python code style

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -13,6 +13,6 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         return self.__height * self.__width
-    
+
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
