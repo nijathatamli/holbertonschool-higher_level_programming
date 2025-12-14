654f09dc303d41221ec80fe568e05e9532552b68
Nijat <hatamlinijat@gmail.com>
2025-12-14 21:38:35 +0400

    added some function in Rectangle class

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class contain two functions which are __init__,__str__ ,area()"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """__init__ function create private values"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+    
+    def __str__(self):
+        return f"[Recatangle] {self.__width}/{self.__height}"
