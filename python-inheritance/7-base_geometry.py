151d157341c5295fec834dd61cfd3046959719d1
Nijat <hatamlinijat@gmail.com>
2025-12-14 16:32:43 +0400

    BaseGearmtry functions enhances

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Modul containing integer validator and area fucntions"""
+
+
+class BaseGeometry:
+    """Class for BaseGeometry"""
+    def area(self):
+        """Method to raise exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Method to validate integer"""
+        if type(value) is not int:
+            raise TypeError(name + "must be integer")
+        if value <= 0:
+            raise ValueError(name + "name must be greater than 0")
