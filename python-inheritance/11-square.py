648607fb4f00cb6aa4a9323474e3f85c4d8ba536
Nijat <hatamlinijat@gmail.com>
2025-12-15 20:41:14 +0400

    python code stye is complated

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-    
+
     def area(self):
         return self.__size ** 2
-    
+
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"
