d0e88e96eb4b3e27c49a448664f3c6e324f0b605
Nijat <hatamlinijat@gmail.com>
2025-12-15 11:55:02 +0400

    Square class contain 2 functions which init inheritence rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class contains 2 functions"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """area() fucntions eixsts area of square"""
+
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
