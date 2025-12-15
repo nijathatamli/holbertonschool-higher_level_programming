f35f21a5c6c1070aa19047f9a0516590219a82da
Nijat <hatamlinijat@gmail.com>
2025-12-15 20:39:31 +0400

    documented 11th python code

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""Square class contains 3 functions, both of them are dunder methods"""
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """area print area of square, others are dunder"""
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
