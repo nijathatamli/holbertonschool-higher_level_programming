ec7cf617444db6a9e9272e9484bf4a3261a0f6a0
root <hatamlinijat@gmail.com>
2026-01-08 09:08:26 +0000

    test

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 0
-    magic_string.count += 1
-    return ", ".join(["BestSchool"] * magic_string.count)
+    magic_string.count =  getattr(magic_string.count, "count", 0) + 1
+    return ", ".join(magic_string.count * ["Best School"])
