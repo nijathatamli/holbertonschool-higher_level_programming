fdb4beafa5dbc2ebc3a317ff2cfbc1f9b3e9f771
Nijat <hatamlinijat@gmail.com>
2026-03-02 05:56:40 +0400

    fixed bug

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+import json
+
 """
-This module for Write to a file task
+This module for Create a basic serialization module that
+adds the functionality to serialize a Python dictionary
+to a JSON file and deserialize the JSON file to recreate
+the Python Dictionary.
 """
 
 
-def write_file(filename="", text=""):
-    """This function for writes a string to a text file"""
-    with open(filename, 'w', encoding="UTF8") as file:
-        return file.write(text)
+def serialize_and_save_to_file(data, filename):
+    """Function for serialize and save data to the specified file """
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """Function for  load and deserialize data from the specified file"""
+    with open(filename, 'r') as file:
+        return json.load(file)
